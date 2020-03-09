58f0669dba5402676f71db70192998d7dbe5fdbc
czz78 <luca.cuzzolin@gmail.com>
2020-03-09 18:56:43 +0100

    Update tivix.js
    small fixes

--- tivix.js
+++ tivix.js
@@ -1,7 +1,7 @@
 /**
  * FMovies plugin for Movian Media Center
  *
- *  Copyright (C) 2018 lprot
+ *  Copyright (C) 2020 czz78
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
